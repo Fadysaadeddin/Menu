eed73d339779d96f0e38ce5654d8056a2cc70458
fady <fadysaadeddin@gmail.com>
2025-06-14 05:17:10 +0200

    developing a feature

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,6 +1,7 @@
-
+import React from "react";
 import HomeHeader from "../components/homeHeader";
 import Category from "../components/Category";
+import SubCategory from "../components/SubCategory";
 import "../styles/home.css";
 
 const Home = () => {
@@ -8,9 +9,8 @@ const Home = () => {
     <div className="home-container">
       <HomeHeader />
       <Category />
-      <main className="main-content">
-   
-      </main>
+      <SubCategory />
+      <main className="main-content"></main>
     </div>
   );
 };
